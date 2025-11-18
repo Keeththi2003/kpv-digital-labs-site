53b8d9ff597e2ad7e482a1f207e753eb501618b9
Keeththi2003 <keeththi2003@gmail.com>
2025-11-18 13:59:20 +0530

    fix: adjust background class for consistency in Home component layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from "./components/home/hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center  font-sans dark:bg-black ">
       <HeroSection/>
     </div>
   );
