06c901634c38f1e1ab6054e51d2a0fd4bd24e3b1
Keeththi2003 <keeththi2003@gmail.com>
2025-11-19 11:05:47 +0530

    feat: add TrustedBy component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { HeroSection } from "./components/home/hero";
 import { TechStack } from "./components/home/TechStack";
 import { CTASection } from "./components/CTASection";
+import { TrustedBy } from "./components/home/TrustedBy";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <HeroSection/>
       <TechStack></TechStack>
+      <TrustedBy/>
       <CTASection/>
     </div>
   );
