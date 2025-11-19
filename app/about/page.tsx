ac8adb7232a0199dd7a8c8ff0540e940c0e0e453
Keeththi2003 <keeththi2003@gmail.com>
2025-11-19 21:44:37 +0530

    refactor: remove unused imports from Home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import { HeroSection } from "@/app/components/home/hero";
-import { TechStack } from "@/app/components/home/TechStack";
+
 import { CTASection } from "@/app/components/CTASection";
 import {Hero} from "@/app/about/Hero";
-import { TrustedBy } from "@/app/components/home/TrustedBy";
 
 export default function Home() {
   return (
