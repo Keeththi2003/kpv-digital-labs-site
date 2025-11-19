3905329bde52ac7850828a85159d46bcf47d6594
Keeththi2003 <keeththi2003@gmail.com>
2025-11-19 10:16:27 +0530

    feat: add CTASection component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { HeroSection } from "./components/home/hero";
 import { TechStack } from "./components/home/TechStack";
+import { CTASection } from "./components/CTASection";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <HeroSection/>
       <TechStack></TechStack>
+      <CTASection/>
     </div>
   );
 }
