47f985166cc17844d6be5fdc15b7c4b692420811
Keeththi2003 <keeththi2003@gmail.com>
2025-11-22 10:58:52 +0530

    feat: remove Hero component and update page structure to include WorkSteps for improved project workflow presentation

--- app/how-we-work/page.tsx
+++ app/how-we-work/page.tsx
@@ -1,11 +1,13 @@
 
 import { CTASection } from "@/app/components/CTASection";
-import {Hero} from "@/app/work/Hero";
+import {Hero} from "@/app/how-we-work/Hero";
+import WorkSteps from "@/app/how-we-work/WorkSteps";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <Hero/>
+      <WorkSteps/>
       <CTASection/>
     </div>
   );
