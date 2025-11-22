9e3b8f643127f31f1373d24b793bb79e206baf2d
Keeththi2003 <keeththi2003@gmail.com>
2025-11-22 11:27:24 +0530

    feat: add FAQ component to enhance user experience and provide project-related information

--- app/how-we-work/page.tsx
+++ app/how-we-work/page.tsx
@@ -2,12 +2,14 @@
 import { CTASection } from "@/app/components/CTASection";
 import {Hero} from "@/app/how-we-work/Hero";
 import WorkSteps from "@/app/how-we-work/WorkSteps";
+import FAQ from "./FAQ";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <Hero/>
       <WorkSteps/>
+      <FAQ/>
       <CTASection/>
     </div>
   );
