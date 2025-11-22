9c09f72517e7d38a6e2ba9ee106212a3eca93218
Keeththi2003 <keeththi2003@gmail.com>
2025-11-22 16:23:15 +0530

    feat: integrate FAQ component into Home page with initial data structure

--- app/services/artificial-intelligence/page.tsx
+++ app/services/artificial-intelligence/page.tsx
@@ -1,10 +1,27 @@
 import { CTASection } from "@/app/components/CTASection";
 import {Hero} from "@/app/services/artificial-intelligence/Hero";
+import FAQ from "@/app/components/FAQ";
+
+const faqs = {
+  subtitle: "",
+  faqs:[
+    {
+      question:"",
+      answer:""
+    },
+    {
+      question:"",
+      answer:""
+    },
+
+  ]
+}
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <Hero/>
+      <FAQ faqs={faqs.faqs} subtitle={faqs.subtitle}/>
       <CTASection/>
     </div>
   );
