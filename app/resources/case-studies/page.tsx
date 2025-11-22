0de528ccc274ecc09501401b80ba3524375ca87b
Keeththi2003 <keeththi2003@gmail.com>
2025-11-23 00:44:58 +0530

    feat: add CaseStudies component to Home page for enhanced content display

--- app/resources/case-studies/page.tsx
+++ app/resources/case-studies/page.tsx
@@ -1,11 +1,13 @@
 
 import { CTASection } from "@/app/components/CTASection";
 import {Hero} from "@/app/resources/case-studies/Hero";
+import CaseStudies from "./CaseStudies";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <Hero/>
+      <CaseStudies/>
       <CTASection/>
     </div>
   );
