9fea3cdb35aa21a0c397b9492d42c33f212ef96d
Keeththi2003 <keeththi2003@gmail.com>
2025-11-23 16:49:52 +0530

    feat: add OurBenifits component to showcase services with animations

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,12 +1,14 @@
 import { CTASection } from "@/app/components/CTASection";
 import {Hero} from "@/app/careers/Hero";
 import LifeAtHere from "@/app/careers/LifeAtHere";
+import OurBenifits from "./OurBenifits";
 
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center  font-sans dark:bg-black ">
       <Hero/>
       <LifeAtHere/>
+      <OurBenifits/>
       <CTASection/>
     </div>
   );
