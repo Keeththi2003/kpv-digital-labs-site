4f3025dd1cabea2c6d838309e3f231f5dde193e0
Keeththi2003 <keeththi2003@gmail.com>
2025-11-30 20:53:27 +0530

    feat: add HeroProps type and update Hero component to accept title and description as props

--- app/projects/[id]/Hero.tsx
+++ app/projects/[id]/Hero.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-export function Hero() {
+
+type HeroProps = {
+    title:string,
+    discribtion:string,
+}
+
+export function Hero({title,discribtion}:HeroProps) {
   return (
     <section className="flex items-start justify-center px-4 pt-24 md:pt-32 pb-20 relative">
       <div className="max-w-4xl mx-auto text-center relative z-10">
@@ -10,16 +16,14 @@ export function Hero() {
         </div>
         <div className="space-y-8">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white leading-tight text-balance animate-fade-in-heading">
-            Project Based Development{" "}
+            {title}{" "}
           </h1>
 
           <div className="animate-fade-in-subheading">
             {/* Desktop version - single line */}
             <div className=" sm:flex items-center justify-center gap-2 sm:gap-3 flex-wrap">
               <span className="text-xl md:text-2xl max-w-4xl text-slate-300">
-                We partner closely with you — from discovery and planning to delivery and
-                ongoing support — to build reliable, scalable software that meets your
-                goals on time and within budget.
+                {discribtion}
               </span>
             </div>
           </div>
